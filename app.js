3d2698af03ab984be13801aee8e3e96254f3e383
Jannis R <mail@jannisr.de>
2017-05-01 20:11:27 +0200

    prepare for Docker: drop HTTPS

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 
 const config = require('config')
 const express = require('express')
-const forceSSL = require('express-force-ssl')
 const compression = require('compression')
 const files = require('serve-static')
 const path = require('path')
@@ -14,8 +13,6 @@ const departures = require('./departures')
 
 
 const app = express()
-app.set('forceSSLOptions', {httpsPort: config.ports.https})
-app.use(forceSSL)
 app.use(compression())
 app.use(files(path.join(__dirname, 'client')))
 
