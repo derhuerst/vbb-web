9a1ec26e2e9c05de4a7c618e03fd2ff8746c7721
Jannis R <mail@jannisr.de>
2018-11-05 21:44:54 +0100

    server: vbb-client -> bvg-hafas

--- departures.js
+++ departures.js
@@ -1,19 +1,18 @@
 'use strict'
 
+const createHafas = require('bvg-hafas')
 const stations = require('vbb-stations')
-const api = require('vbb-client')
-
 const template = require('./tpl/departures')
+
+const hafas = createHafas('vbb-web')
 
 const departures = (req, res) => {
 	if (!req.query.station) return res.status(400).end('missing station')
 	const station = stations(req.query.station)[0]
 	if (!station) return res.status(400).end('invalid station')
 
-	const when = req.query.when || new Date()
-
-	api.departures(station.id, {
-		when,
+	hafas.departures(station.id, {
+		when: req.query.when ? +new Date(req.query.when) : Date.now(),
 		identifier: 'vbb-web'
 	})
 	.then((deps) => {
