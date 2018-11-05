c4e6861d6a1b196280aa67f7b9993ca3614d67d0
Jannis R <mail@jannisr.de>
2018-11-05 20:58:25 +0100

    client: 2.vbb.transport.rest -> 1.bvg.transport.rest

--- client/lib/stations.js
+++ client/lib/stations.js
@@ -1,13 +1,19 @@
 'use strict'
 
-const api = require('vbb-client')
+const createClient = require('vbb-client')
+
+const client = createClient({
+	endpoint: 'https://1.bvg.transport.rest'
+})
 
 const stations = (query, cb) => {
 	if (query.length === 0) return Promise.resolve([])
 
-	return api.stations({
-		query,
+	return client.locations(query, {
 		fuzzy: true,
+		poi: false,
+		addresses: false,
+		stationLines: false,
 		results: 5,
 		identifier: 'vbb-web'
 	})
